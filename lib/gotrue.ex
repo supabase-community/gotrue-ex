d899bb4c5820c0f13d64465e53178966534605ed
Josh Nussbaum <joshnuss@gmail.com>
2021-01-09 02:52:39 -0500

    Adds GoTrue.sign_up/1

--- lib/gotrue.ex
+++ lib/gotrue.ex
@@ -9,10 +9,40 @@ defmodule GoTrue do
   plug Tesla.Middleware.JSON
 
   @doc "Get environment settings for the GoTrue server"
-  @type settings() :: map
+  @spec settings() :: map
   def settings do
     {:ok, %{status: 200, body: json}} = get("/settings")
 
     json
+  end
+
+  @doc "Sign up a new user with email and password"
+  @spec sign_up(%{
+          required(:email) => String.t(),
+          required(:password) => String.t(),
+          data: map(),
+          audience: String.t(),
+          provider: String.t()
+        }) :: map
+  def sign_up(credentials) do
+    payload =
+      credentials
+      |> Map.take([:email, :password, :data, :provider])
+      |> Map.merge(%{aud: credentials[:audience]})
+
+    case post("/signup", payload) do
+      {:ok, %{status: 200, body: json}} ->
+        {:ok,
+         %{
+           access_token: json["access_token"],
+           expires_in: json["expires_in"],
+           token_type: json["token_type"],
+           refresh_token: json["refresh_token"],
+           user: json["user"]
+         }}
+
+      {:ok, %{status: status, body: json}} ->
+        {:error, %{code: status, message: json["msg"]}}
+    end
   end
 end
--- test/gotrue_test.exs
+++ test/gotrue_test.exs
@@ -15,4 +15,98 @@ defmodule GoTrueTest do
   test "settings/0" do
     assert GoTrue.settings() == %{"a_setting" => 1234}
   end
+
+  describe "sign_up/1" do
+    test "with invalid response" do
+      mock(fn
+        %{method: :post, url: "http://auth.example.com/signup"} ->
+          json(%{"msg" => "invalid"}, status: 422)
+      end)
+
+      assert GoTrue.sign_up(%{email: "user@example.com", password: "oops"}) ==
+               {:error, %{code: 422, message: "invalid"}}
+    end
+
+    test "with duplicate response" do
+      mock(fn
+        %{method: :post, url: "http://auth.example.com/signup"} ->
+          json(%{"msg" => "already exists"}, status: 400)
+      end)
+
+      assert GoTrue.sign_up(%{email: "exists@example.com", password: "12345"}) ==
+               {:error, %{code: 400, message: "already exists"}}
+    end
+
+    test "with valid params" do
+      mock(fn
+        %{
+          method: :post,
+          url: "http://auth.example.com/signup",
+          body:
+            ~s|{"aud":null,"data":{"favorite_language":"elixir"},"email":"user@example.com","password":"12345"}|
+        } ->
+          json(
+            %{
+              access_token: "1234",
+              expires_in: 60,
+              token_type: "bearer",
+              refresh_token: "abcd",
+              user: %{}
+            },
+            status: 200
+          )
+      end)
+
+      assert GoTrue.sign_up(%{
+               email: "user@example.com",
+               password: "12345",
+               data: %{favorite_language: "elixir"}
+             }) ==
+               {:ok,
+                %{
+                  access_token: "1234",
+                  expires_in: 60,
+                  refresh_token: "abcd",
+                  token_type: "bearer",
+                  user: %{}
+                }}
+    end
+
+    test "with extra params" do
+      mock(fn
+        %{
+          method: :post,
+          url: "http://auth.example.com/signup",
+          body:
+            ~s|{"aud":"superheros","data":{"favorite_language":"elixir"},"email":"user@example.com","password":"12345","provider":"email"}|
+        } ->
+          json(
+            %{
+              access_token: "1234",
+              expires_in: 60,
+              token_type: "bearer",
+              refresh_token: "abcd",
+              user: %{}
+            },
+            status: 200
+          )
+      end)
+
+      assert GoTrue.sign_up(%{
+               email: "user@example.com",
+               password: "12345",
+               data: %{favorite_language: "elixir"},
+               audience: "superheros",
+               provider: "email"
+             }) ==
+               {:ok,
+                %{
+                  access_token: "1234",
+                  expires_in: 60,
+                  refresh_token: "abcd",
+                  token_type: "bearer",
+                  user: %{}
+                }}
+    end
+  end
 end
