79d0c38e1323a412b1673147c2569b5cf88023b0
Josh Nussbaum <joshnuss@gmail.com>
2021-01-09 07:08:50 -0500

    Prepare for release

--- mix.exs
+++ mix.exs
@@ -1,13 +1,21 @@
 defmodule GoTrue.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/joshnuss/gotrue-elixir"
+
   def project do
     [
       app: :gotrue,
       version: "0.1.0",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      docs: docs(),
+      package: [
+        maintainers: ["Joshua Nussbaum"],
+        licenses: ["MIT"],
+        links: %{GitHub: @source_url}
+      ],
     ]
   end
 
@@ -18,12 +26,21 @@ defmodule GoTrue.MixProject do
     ]
   end
 
+  defp docs do
+    [
+      main: "readme",
+      source_url: @source_url,
+      extras: ["README.md"]
+    ]
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:tesla, "~> 1.4"},
       {:hackney, "~> 1.16.0"},
-      {:jason, ">= 1.0.0"}
+      {:jason, ">= 1.0.0"},
+      {:ex_doc, github: "elixir-lang/ex_doc", only: :dev}
     ]
   end
 end
