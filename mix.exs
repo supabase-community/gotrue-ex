232253c014775c7220d87a2d348e2cdf29385657
Josh Nussbaum <joshnuss@gmail.com>
2021-01-09 08:29:27 -0500

    Bump version

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule GoTrue.MixProject do
   def project do
     [
       app: :gotrue,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
